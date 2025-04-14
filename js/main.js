a5158a0a604c6f93c682abb89b1b438a9ebe0751
takeru-k <kam41893@gmail.com>
2025-04-14 13:58:23 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+    $('.l-header__nav, .c-hamburger__line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+  })
+
+});
+
+// toggle
+$(function(){
+	$('.c-toggle__header').click(function(){
+		$(this).toggleClass('selected');
+		$(this).next().slideToggle();
+	});
+});
+
+
+const mySwiper = new Swiper('.swiper', {
+  // Optional parameters
+
+   slidesPerView: 2.4,
+   
+   breakpoints: {
+    // スライドの表示枚数：500px以上の場合
+    800: {
+      slidesPerView: 5.5,
+      spaceBetween: 60,
+    }
+  },
+  spaceBetween: 16,
+  
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+ 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
